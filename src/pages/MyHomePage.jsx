e2b5ab32af43e082efad4ffbd1c5d92f9079f153
HellgrenR <rallenolltva@gmail.com>
2024-02-26 12:01:41 +0100

    Added some text

--- src/pages/MyHomePage.jsx
+++ src/pages/MyHomePage.jsx
@@ -4,17 +4,21 @@ function MyHomePage() {
 
   return (
     <div className="homepage-container">
-      <HomePageSlider/>
+      <HomePageSlider />
 
       <div className="homepage-desc-and-revs">
         <div className="homepage-description">
-
+          <p>MysteryInc är en auktionssida där du budar på mystery boxes med olika teman!</p>
         </div>
-
         <div className="homepage-reviews">
-
+          <div className="homepage-reviews-title-score">
+            <h1 className="homepage-reviews-title">Väldigt najs!!</h1>
+            <h1 className="homepage-reviews-score">★★★★☆</h1>
+          </div>
+          <p>Budade på en djurlåda, och fick en hel häst!</p>
         </div>
       </div>
+
     </div>
   )
 }
