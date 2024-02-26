a6032f795013de9641e2fbbfbd03cadeb39a9c24
HellgrenR <rallenolltva@gmail.com>
2024-02-26 13:59:03 +0100

    added css

--- src/components/HomePageSlider.jsx
+++ src/components/HomePageSlider.jsx
@@ -2,7 +2,9 @@
 function HomePageSlider() {
 
   return (
-    <></>
+    <div className="homepage-slider">
+      <img src="https://www.ikea.com/se/en/images/products/vattentrag-box-with-lid__1176294_pe895907_s5.jpg?f=s" />
+    </div>
   )
 }
 
