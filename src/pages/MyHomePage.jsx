69550573a34a31f4a594c752e33e81c6b98a069d
HellgrenR <rallenolltva@gmail.com>
2024-02-26 11:26:34 +0100

    Setting up divs

---
+++ src/pages/MyHomePage.jsx
@@ -0,0 +1,21 @@
+
+function MyHomePage() {
+
+  return (
+    <div className="homepage-container">
+      <div className="homepage-slider">
+
+      </div>
+      <div className="homepage-desc-and-revs">
+        <div className="homepage-description">
+
+        </div>
+        <div className="homepage-reviews">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MyHomePage
