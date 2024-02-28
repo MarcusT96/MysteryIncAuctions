5c11770a0f085cb00c5905f08d545b25eada1b51
HellgrenR <rallenolltva@gmail.com>
2024-02-28 10:43:21 +0100

    Fixed nav

--- src/components/TempNav.jsx
+++ src/components/TempNav.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
 
 function TempNav() {
-  <nav>
-    <Link to="/">TempHome</Link>
-    <Link to="/ObjectPage">ObjectPage</Link>
-  </nav>
+  return (
+    <nav>
+      <Link to="/">TempHome</Link>
+      <Link to="/ObjectPage">ObjectPage</Link>
+    </nav>
+  )
 }
 
 export default TempNav
