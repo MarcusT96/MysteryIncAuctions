91136d03c2a5ba8b5f2ae736709ddf6f1c05afb6
MarcusT96 <marcus.thure@hotmail.com>
2024-03-04 11:32:16 +0100

    Added button for input for bids

---
+++ src/components/BidPopUp.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function BidPopUp({ onClose, onConfirm }) {
+  const [bid, setBid] = useState('');
+
+  return (
+    <div className="bid-modal-background">
+      <div className="bid-modal-content">
+        <input
+          className="bid-modal-input"
+          type="number"
+          value={bid}
+          onChange={(e) => setBid(e.target.value)}
+          placeholder="Ange ditt bud"
+        />
+        <button
+          className="bid-modal-button"
+          onClick={() => onConfirm(bid)}
+        >
+          Bekräfta bud
+        </button>
+        <button
+          className="bid-modal-close-button"
+          onClick={onClose}
+        >
+          Stäng
+        </button>
+      </div>
+    </div>
+  );
+}
