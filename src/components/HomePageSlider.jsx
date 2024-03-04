3379517f6e1d44fc7d3bafe59e42b3715bd408b0
HellgrenR <rallenolltva@gmail.com>
2024-03-04 14:02:10 +0100

    Trying to create carousel
    IT DOESN'T WORK :(

--- src/components/HomePageSlider.jsx
+++ src/components/HomePageSlider.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 function HomePageSlider() {
   const [boxes, setBoxes] = useState([])
-  const [selectedBoxIndex, setSelectedBoxIndex] = useState(0)
   const navigate = useNavigate()
 
+  // Load boxes
   useEffect(() => {
     async function load() {
       const response = await fetch('Mysteryboxes.json')
@@ -15,51 +18,29 @@ function HomePageSlider() {
     load()
   }, [])
 
-  useEffect(() => {
-    const autoPlayInterval = setInterval(handleNextButtonClick, 1000)
-    return () => {
-      clearInterval(autoPlayInterval)
-    }
-  }, [])
-
+  // Navigate to auction page
   const navigateToObjectPage = (id) => {
     navigate(`/box/${id}`);
   }
 
-  const handlePrevButtonClick = () => {
-    setSelectedBoxIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : boxes.length - 1))
-  }
-
-  const handleNextButtonClick = () => {
-    setSelectedBoxIndex((prevIndex) => (prevIndex < boxes.length - 1 ? prevIndex + 1 : 0))
-  }
-
-  const getDisplayedBoxes = () => {
-    const endIndex = selectedBoxIndex + 3
-    const slicedBoxes = boxes.slice(selectedBoxIndex, endIndex + 1)
-
-    if (slicedBoxes.length < 4) {
-      const remainingBoxes = 4 - slicedBoxes.length
-      return slicedBoxes.concat(boxes.slice(0, remainingBoxes))
-    }
-
-    return slicedBoxes
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
   }
 
   return (
     <div className="homepage-slider">
-      <button className="homepage-slidebutton" onClick={handlePrevButtonClick}>
-        ❮
-      </button>
-      {getDisplayedBoxes().map((box, index) => (
-        <div className="homepage-slider-display" key={index}>
-          <img src={box.image} onClick={() => navigateToObjectPage(box.id)} style={{ cursor: 'pointer' }} />
-          <h2>{box.name}</h2>
-        </div>
-      ))}
-      <button className="homepage-slidebutton" onClick={handleNextButtonClick}>
-        ❯
-      </button>
+      <Slider {...settings}>
+        {boxes.map((box, index) => (
+          <div className="homepage-slider-display" key={index}>
+            <img src={box.image} onClick={() => navigateToObjectPage(box.id)} style={{ cursor: 'pointer' }} />
+            <h2>{box.name}</h2>
+          </div>
+        ))}
+      </Slider>
     </div>
   )
 }
