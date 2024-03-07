e905966487dfdf49556b5a0a351a9f06b986ec2e
HellgrenR <rallenolltva@gmail.com>
2024-03-07 12:17:31 +0100

    now reads from db

--- src/components/orderHistory.jsx
+++ src/components/orderHistory.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
 
 function OrderHistory() {
+
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`http://localhost:3000/bought_boxes`)
+      const data = await response.json()
+      setOrders(data)
+    }
+    load()
+  }, [])
+
   return (
-    <h1>hej</h1>
+    <div className="orderhistory-container">
+      {orders.map((order, index) => (
+        <div className="orderhistory-order" key={index}>
+          <img src={order.image} />
+          <h3>{order.name}</h3>
+          <p>{order.price}kr</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
