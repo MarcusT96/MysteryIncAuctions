dabfcecf67b07fb6ebc9f71edfa4d461c5e7e09d
loken156 <jonathan.karlsson@mail.com>
2024-03-07 10:51:26 +0100

    commented some code and fixed small css

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,42 +19,42 @@ function Products({ searchQuery, sortOrder, sortCriterion }) {
   }, []);
 
   useEffect(() => {
-    const now = new Date()
+    const now = new Date() // skapar en current time variabel
     const filtered = items.filter(item =>
       item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      item.description.toLowerCase().includes(searchQuery.toLowerCase())
+      item.description.toLowerCase().includes(searchQuery.toLowerCase()) // searchquery till lowercase som filtrerar det du skriver i searchbaren
     );
 
-    const sortedItems = [...filtered].sort((a, b) => {
+    const sortedItems = [...filtered].sort((a, b) => { // Sorterings funktion
       if (sortCriterion === 'price') {
-        return sortOrder === "asc" ? a.price - b.price : b.price - a.price;
+        return sortOrder === "asc" ? a.price - b.price : b.price - a.price; // jämför pris a - pris b eller tvärtom
       
       } else if (sortCriterion === 'time') {
       
-        const endTimeA = new Date(a.time).getTime()
+        const endTimeA = new Date(a.time).getTime() // Här skapar jag endtime till millisekunder för att kunna jämföra dom
         const endTimeB = new Date(b.time).getTime()
 
-        const timeLeftA = endTimeA - now;
+        const timeLeftA = endTimeA - now; // Räknar ut hur lång tid det är kvar exakt från nu till sluttiden
         const timeLeftB = endTimeB - now
 
-        return sortOrder === "asc" ? timeLeftA - timeLeftB : timeLeftB - timeLeftA
+        return sortOrder === "asc" ? timeLeftA - timeLeftB : timeLeftB - timeLeftA // Returnerar ny ordning i tidsföljd
      
       } else if (sortCriterion === 'name') {
-       return sortOrder === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+       return sortOrder === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name); //Sorterar ut i alfabetisk ordning, båda riktningar
       }
-      return 0; // Default case if no sortCriterion matches
+      return 0; // Default värde om inget matchar
     });
 
-    setFilteredItems(sortedItems);
-  }, [searchQuery, items, sortOrder, sortCriterion]);
+    setFilteredItems(sortedItems); // updaterare state
+  }, [searchQuery, items, sortOrder, sortCriterion]); // mina useffect värden, när dessa värden ändras så renderar den om
 
 
 
   const navigateToObjectPage = (id) => {
-    navigate(`/box/${id}`);
+    navigate(`/box/${id}`); // Navigation till objectpage
   };
 
-  return (
+  return ( // Hela auctions kortet renderas här, den kör så många den kan baserat på hur många idn där finns 
     <article className="Auction-list">
       {filteredItems.map(item => (
         <section key={item.id} className="auction-card">
