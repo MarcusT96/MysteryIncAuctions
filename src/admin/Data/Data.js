00d269a391c118c851cff78eab21085ec296a594
Tommy <ttommywang@gmail.com>
2024-03-07 09:10:19 +0100

    added data configurations for application use

---
+++ src/admin/Data/Data.js
@@ -0,0 +1,30 @@
+import {
+    VscHome,
+    VscOutput,
+    VscOrganization,
+    VscPackage,
+    VscGraph
+} from "react-icons/vsc";
+
+export const adminIcons = [
+    {
+        icon: VscHome,
+        heading: "Startsida",
+    },
+    {
+        icon: VscOutput,
+        heading: "Beställningar",
+    },
+    {
+        icon: VscOrganization,
+        heading: "Användare",
+    },
+    {
+        icon: VscPackage,
+        heading: "Produkter",
+    },
+    {
+        icon: VscGraph,
+        heading: "Rapporter",
+    },
+];
