7a8c44abaa421a8fe76d6d778f3be337a49259c8
MarcusT96 <marcus.thure@hotmail.com>
2024-03-08 11:56:41 +0100

    Adderar kategorier till Products och Objectpage

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,6 +5,7 @@ import CountdownTimer from "./CountDownTimer.jsx";
 function Products({ searchQuery, sortOrder, sortCriterion }) {
   const [items, setItems] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
+  const [categories, setCategories] = useState({});
   const navigate = useNavigate();
 
   // Funktion för att korta ner varje box beskrivning på auctionpage men behåller full längd på objectpage
@@ -20,23 +21,34 @@ function Products({ searchQuery, sortOrder, sortCriterion }) {
       const response = await fetch(`http://localhost:3000/mystery_boxes`)
       const data = await response.json();
       setItems(data);
+
+      const categoriesResponse = await fetch(`http://localhost:3000/categories`);
+      const categoriesData = await categoriesResponse.json();
+      const categoriesMap = categoriesData.reduce((acc, category) => {
+        acc[category.id] = category.categoryName; // Map id to categoryName
+        return acc;
+      }, {});
+      setCategories(categoriesMap);
     }
     load();
   }, []);
 
   useEffect(() => {
     const now = new Date() // skapar en current time variabel
-    const filtered = items.filter(item =>
+    const filtered = items.map(item => ({
+      ...item,
+      categoryName: categories[item.category], // Adderar kategorinamn till varje box
+    })).filter(item =>
       item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
       item.description.toLowerCase().includes(searchQuery.toLowerCase()) // searchquery till lowercase som filtrerar det du skriver i searchbaren
     );
 
-    const sortedItems = [...filtered].sort((a, b) => { // Sorterings funktion
+    const sortedItems = [...filtered].sort((a, b) => { // Sorteringsfunktion
       if (sortCriterion === 'price') {
         return sortOrder === "asc" ? a.price - b.price : b.price - a.price; // jämför pris a - pris b eller tvärtom
-      
+
       } else if (sortCriterion === 'time') {
-      
+
         const endTimeA = new Date(a.time).getTime() // Här skapar jag endtime till millisekunder för att kunna jämföra dom
         const endTimeB = new Date(b.time).getTime()
 
@@ -44,15 +56,15 @@ function Products({ searchQuery, sortOrder, sortCriterion }) {
         const timeLeftB = endTimeB - now
 
         return sortOrder === "asc" ? timeLeftA - timeLeftB : timeLeftB - timeLeftA // Returnerar ny ordning i tidsföljd
-     
+
       } else if (sortCriterion === 'name') {
-       return sortOrder === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name); //Sorterar ut i alfabetisk ordning, båda riktningar
+        return sortOrder === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name); //Sorterar ut i alfabetisk ordning, båda riktningar
       }
       return 0; // Default värde om inget matchar
     });
 
     setFilteredItems(sortedItems); // updaterare state
-  }, [searchQuery, items, sortOrder, sortCriterion]); // mina useffect värden, när dessa värden ändras så renderar den om
+  }, [searchQuery, items, sortOrder, sortCriterion, categories]); // mina useffect värden, när dessa värden ändras så renderar den om
 
 
 
@@ -65,6 +77,7 @@ function Products({ searchQuery, sortOrder, sortCriterion }) {
       {filteredItems.map(item => (
         <section key={item.id} className="auction-card">
           <h2 className="auction-title">{item.name}</h2>
+          <p className="category-name">Kategori: {item.categoryName}</p>
           <p>{truncateString(item.description, 100)}</p>
           <img src={item.image} alt={item.name} />
           <p> Tid kvar: <h2 className="tidtag">{<CountdownTimer endTime={item.time} />}</h2>  </p>
