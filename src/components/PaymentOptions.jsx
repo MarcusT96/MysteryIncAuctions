2b40776b7ff1f1ecd897e7844e513153ebf66f8d
HellgrenR <rallenolltva@gmail.com>
2024-03-08 14:40:29 +0100

    Reads payment methods from db

--- src/components/PaymentOptions.jsx
+++ src/components/PaymentOptions.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function PaymentOptions() {
+
+  const [paymentInfo, setPaymentInfo] = useState([])
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`http://localhost:3000/payment_options`)
+      const data = await response.json()
+      let paymentData = []
+      for (let method of data) {
+        if (method.user_id == localStorage.currentUserId) {
+          paymentData.push(method)
+        }
+      }
+      setPaymentInfo(paymentData)
+    }
+    load()
+  }, [])
+
   return (
     <div>
       <h2>Betalningsalternativ</h2>
       <p>Här kan du hantera dina betalningsalternativ.</p>
+      <button className='paymentopt-addpay'>Lägg till betalningsalternativ</button>
+      <div className='paymentopt-method-container'>
+        {paymentInfo.map((payment, index) => (
+          <div className='paymentopt-method' key={index}>
+            <h2 className='paymentopt-type'>{payment.type}</h2>
+          </div>
+        ))}
+      </div>
       {/* Här lägger vi till logik och UI-element för betalningsalternativen */}
     </div>
   );
