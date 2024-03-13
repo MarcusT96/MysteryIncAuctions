5b5e8c0ac4d76fee846aaf5f2676f62220549d8e
HellgrenR <rallenolltva@gmail.com>
2024-03-13 12:31:11 +0100

    Added cards and styling

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,6 +1,15 @@
+import { useState, useEffect } from 'react'
 import Modal from 'react-modal'
 
 function Payment({ isOpen, onClose, order }) {
+
+  const [cardType, setCardType] = useState("")
+  const [cardNumber, setCardNumber] = useState("")
+  const [expDate, setExpDate] = useState("")
+  const [cardCvc, setCardCvc] = useState("")
+  const [cardHolder, setCardHolder] = useState("")
+
+  const [paymentInfo, setPaymentInfo] = useState([])
 
   const customStyles = {
     content: {
@@ -20,13 +29,79 @@ function Payment({ isOpen, onClose, order }) {
     }
   }
 
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`/api/payment_options`)
+      const data = await response.json()
+      let paymentData = []
+      for (let method of data) {
+        if (method.user_id == localStorage.currentUserId) {
+          paymentData.push(method)
+        }
+      }
+      setPaymentInfo(paymentData)
+    }
+    load()
+  }, [])
+
+  function censorCard(cardNumber) {
+    let censored = cardNumber.slice(0, 4)
+    censored = censored + "-XXXX-XXXX-XXXX"
+    return censored
+  }
 
   return (
     <Modal isOpen={isOpen}
       onRequestClose={onClose}
       style={customStyles}>
       <h2>Betalning</h2>
-      <p>{order.price}</p>
+
+      <div className='pay-order-options'>
+        <div className='pay-order'>
+          <h3>{order.name}</h3>
+          <p>Kostnad: {order.price}kr</p>
+        </div>
+        <div className='pay-options'>
+          {paymentInfo.map((payment, index) => (
+            <div className='pay-card'>
+              <p>{payment.type}</p>
+              <p>{censorCard(payment.card_number)}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='pay-details'>
+        <p className='paymentopt-modal-text'>Typ</p>
+        <select className="paymentopt-modal-selector" name="cardtype" id="type">
+          <option value="Credit card">Kredit kort</option>
+          <option value="Debit card">Debit kort</option>
+        </select>
+
+        <p className='paymentopt-modal-text'>Kort nummer</p>
+        <input type="text"
+          value={cardNumber}
+          onChange={(e) => { setCardNumber(e.target.value) }}
+          placeholder='XXXX-XXXX-XXXX-XXXX' />
+
+        <p className='paymentopt-modal-text'>Utgångsdatum</p>
+        <input type="text"
+          value={expDate}
+          onChange={(e) => { setExpDate(e.target.value) }}
+          placeholder='XX-XX' />
+
+        <p className='paymentopt-modal-text'>CVC</p>
+        <input type="text"
+          value={cardCvc}
+          onChange={(e) => { setCardCvc(e.target.value) }}
+          placeholder='XXX' />
+
+        <p className='paymentopt-modal-text'>Kort ägare</p>
+        <input type="text"
+          value={cardHolder}
+          onChange={(e) => { setCardHolder(e.target.value) }}
+          placeholder='' />
+      </div>
       <button className="paymentopt-modal-button-finish" onClick={onClose}>Klar</button>
       <button className="paymentopt-modal-button-cancel" onClick={onClose}>Avbryt</button>
     </Modal>
