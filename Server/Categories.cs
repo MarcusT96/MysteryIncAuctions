2c56be2a4ef37f55ea2f1c6721e0e3848cfba6ec
TommyJX <ttommywang@gmail.com>
2024-04-04 15:42:42 +0200

    Added GetCategories method
    In order to implement a static GetCategories method to the
    CategoryOptions class, which instantiates and returns a list of
    predefined categories.

--- Server/Categories.cs
+++ Server/Categories.cs
@@ -3,4 +3,21 @@ namespace Server;
 public class CategoryOptions
 {
   public record Category(int Id, string CategoryName);
+
+  public static List<Category> GetCategories()
+  {
+    List<Category> result = new()
+    {
+        new(1, "Äventyr"),
+        new(2, "Detektiv"),
+        new(3, "Champion"),
+        new(4, "Pokémon"),
+        new(5, "Mode"),
+        new(6, "Mysterium"),
+        new(7, "Toppval"),
+        new(8, "Teknik")
+    };
+
+    return result;
+  }
 }
