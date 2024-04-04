a746739570c27aac73e0c102efac637d1e84e532
HellgrenR <rallenolltva@gmail.com>
2024-04-04 12:22:42 +0200

    Confused
    I don't like C#

--- Server/paymentopts.cs
+++ Server/paymentopts.cs
@@ -4,5 +4,9 @@ public class PaymentOptions
 {
   public record Cards(int ID, string CardNumber, string ExpDate, string CVC, string Type, string HolderName, string UserID);
 
-  
+  public static List<Cards> PaymentOpts()
+  {
+    List<Cards> result = new();
+    
+  }
 }
