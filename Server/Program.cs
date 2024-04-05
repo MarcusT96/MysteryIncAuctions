66caf88d269b87494e24e8be35024aa2a2fdd19f
HellgrenR <rallenolltva@gmail.com>
2024-04-05 11:51:04 +0200

    Url change

--- Server/Program.cs
+++ Server/Program.cs
@@ -6,6 +6,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/api/categories", CategoryOptions.GetCategories);
-app.MapGet("/payment_options", PaymentOptions.PaymentOpts);
+app.MapGet("/api/payment_options", PaymentOptions.PaymentOpts);
 
 app.Run();
