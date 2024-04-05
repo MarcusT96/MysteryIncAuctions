1558a54b870f1587c7785426d1b9c8b9944bf1ab
HellgrenR <rallenolltva@gmail.com>
2024-04-05 15:04:29 +0200

    Now reads payment options from a JSON list

--- Server/paymentopts.cs
+++ Server/paymentopts.cs
@@ -1,17 +1,90 @@
-namespace Server;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
 
-public class PaymentOptions
+namespace Server
 {
-  public record Cards(int ID, string CardNumber, string ExpDate, string CVC, string Type, string HolderName, string UserID);
-
-  public static List<Cards> PaymentOpts()
+  public class PaymentOptions
   {
-    List<Cards> result = new()
+    public record Cards(string id, string card_number, string expiration_date, string CVC, string type, string cardholder_name, string user_id);
+
+    public static List<Cards> PaymentOpts()
     {
-      new(1, "1111-2222-3333-4444", "12-26", "343", "Credit card", "Åke Jimmiesson", "1")
-    };
+      string paymentJSON = @"
+            [
+                {
+                    ""id"": ""c80f"",
+                    ""card_number"": ""1111-2222-3333-4444"",
+                    ""expiration_date"": ""01-28"",
+                    ""CVC"": ""123"",
+                    ""type"": ""Debit card"",
+                    ""cardholder_name"": ""John Doe"",
+                    ""user_id"": ""0c1f""
+                },
+                {
+                    ""id"": ""7799"",
+                    ""card_number"": ""5555-6666-7777-8888"",
+                    ""expiration_date"": ""05-23"",
+                    ""CVC"": ""456"",
+                    ""type"": ""Credit card"",
+                    ""cardholder_name"": ""Jane Smith"",
+                    ""user_id"": ""1""
+                },
+                {
+                    ""id"": ""73da"",
+                    ""card_number"": ""4656-2343-5436-6453"",
+                    ""expiration_date"": ""05-23"",
+                    ""CVC"": ""456"",
+                    ""type"": ""Debit card"",
+                    ""cardholder_name"": ""Johnny B. Good"",
+                    ""user_id"": ""1""
+                },
+                {
+                    ""id"": ""c03a"",
+                    ""card_number"": ""9876-5432-1098-7654"",
+                    ""expiration_date"": ""09-25"",
+                    ""CVC"": ""789"",
+                    ""type"": ""Debit card"",
+                    ""cardholder_name"": ""Alex Johnson"",
+                    ""user_id"": ""7053""
+                },
+                {
+                    ""id"": ""eb81"",
+                    ""card_number"": ""4321-8765-2109-5432"",
+                    ""expiration_date"": ""12-30"",
+                    ""CVC"": ""321"",
+                    ""type"": ""Credit card"",
+                    ""cardholder_name"": ""Emily White"",
+                    ""user_id"": ""7053""
+                },
+                {
+                    ""id"": ""ef43"",
+                    ""user_id"": ""0c1f"",
+                    ""type"": ""Credit card"",
+                    ""card_number"": ""8345-3249-2345-7654"",
+                    ""expiration_date"": ""04-28"",
+                    ""CVC"": ""485"",
+                    ""cardholder_name"": ""Pelle Jöns""
+                }
+            ]";
 
+      List<Cards> deserializedCards = JsonSerializer.Deserialize<List<Cards>>(paymentJSON);
+      List<Cards> result = new List<Cards>();
 
-    return result;
+      foreach (var card in deserializedCards)
+      {
+        result.Add(new Cards(
+            card.id,
+            card.card_number,
+            card.expiration_date,
+            card.CVC,
+            card.type,
+            card.cardholder_name,
+            card.user_id
+        ));
+      }
+
+      return result;
+    }
   }
 }
