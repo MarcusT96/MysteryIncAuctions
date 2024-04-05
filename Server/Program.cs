ee840797b9089f3df30df5486cb9e7607804cc88
MarcusT96 <marcus.thure@hotmail.com>
2024-04-05 11:39:04 +0200

    Råkade kommentera ut tommys api

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,6 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-//app.MapGet("/api/categories", CategoryOptions.GetCategories);
+app.MapGet("/api/categories", CategoryOptions.GetCategories);
 app.MapGet("/api/bids", Bid.GetBids);
 app.Run();
