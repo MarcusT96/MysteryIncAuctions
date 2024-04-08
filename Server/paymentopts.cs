daa81407f55d8d79e08054457165221d7d0b98ee
HellgrenR <rallenolltva@gmail.com>
2024-04-08 14:20:26 +0200

    Trying to read from DB

--- Server/paymentopts.cs
+++ Server/paymentopts.cs
@@ -1,90 +1,41 @@
 using System;
-using System.Collections.Generic;
-using System.Text.Json;
+using System.Data;
+using MySql.Data.MySqlClient;
 
-namespace Server
+namespace Server;
+
+public class PaymentOptions
 {
-  public class PaymentOptions
+  public record Cards(string id, string card_number, string expiration_date, string CVC, string type, string cardholder_name, string user_id);
+
+  public static List<Cards> PaymentOpts()
   {
-    public record Cards(string id, string card_number, string expiration_date, string CVC, string type, string cardholder_name, string user_id);
+    List<Cards> result = new List<Cards>();
 
-    public static List<Cards> PaymentOpts()
+    using (MySqlConnection conn = new MySqlConnection("server=localhost;port=3306;uid=root;pwd=mypassword;database=mystery_inc"))
     {
-      string paymentJSON = @"
-            [
-                {
-                    ""id"": ""c80f"",
-                    ""card_number"": ""1111-2222-3333-4444"",
-                    ""expiration_date"": ""01-28"",
-                    ""CVC"": ""123"",
-                    ""type"": ""Debit card"",
-                    ""cardholder_name"": ""John Doe"",
-                    ""user_id"": ""0c1f""
-                },
-                {
-                    ""id"": ""7799"",
-                    ""card_number"": ""5555-6666-7777-8888"",
-                    ""expiration_date"": ""05-23"",
-                    ""CVC"": ""456"",
-                    ""type"": ""Credit card"",
-                    ""cardholder_name"": ""Jane Smith"",
-                    ""user_id"": ""1""
-                },
-                {
-                    ""id"": ""73da"",
-                    ""card_number"": ""4656-2343-5436-6453"",
-                    ""expiration_date"": ""05-23"",
-                    ""CVC"": ""456"",
-                    ""type"": ""Debit card"",
-                    ""cardholder_name"": ""Johnny B. Good"",
-                    ""user_id"": ""1""
-                },
-                {
-                    ""id"": ""c03a"",
-                    ""card_number"": ""9876-5432-1098-7654"",
-                    ""expiration_date"": ""09-25"",
-                    ""CVC"": ""789"",
-                    ""type"": ""Debit card"",
-                    ""cardholder_name"": ""Alex Johnson"",
-                    ""user_id"": ""7053""
-                },
-                {
-                    ""id"": ""eb81"",
-                    ""card_number"": ""4321-8765-2109-5432"",
-                    ""expiration_date"": ""12-30"",
-                    ""CVC"": ""321"",
-                    ""type"": ""Credit card"",
-                    ""cardholder_name"": ""Emily White"",
-                    ""user_id"": ""7053""
-                },
-                {
-                    ""id"": ""ef43"",
-                    ""user_id"": ""0c1f"",
-                    ""type"": ""Credit card"",
-                    ""card_number"": ""8345-3249-2345-7654"",
-                    ""expiration_date"": ""04-28"",
-                    ""CVC"": ""485"",
-                    ""cardholder_name"": ""Pelle Jöns""
-                }
-            ]";
+      conn.Open();
+      MySqlCommand cmd = new MySqlCommand("SELECT * from payment_options", conn);
 
-      List<Cards> deserializedCards = JsonSerializer.Deserialize<List<Cards>>(paymentJSON);
-      List<Cards> result = new List<Cards>();
-
-      foreach (var card in deserializedCards)
+      using (MySqlDataReader reader = cmd.ExecuteReader())
       {
-        result.Add(new Cards(
-            card.id,
-            card.card_number,
-            card.expiration_date,
-            card.CVC,
-            card.type,
-            card.cardholder_name,
-            card.user_id
-        ));
+        while (reader.Read())
+        {
+          result.Add(new Cards(
+            reader.GetString("id"),
+            reader.GetString("card_number"),
+            reader.GetString("expiration date"),
+            reader.GetString("CVC"),
+            reader.GetString("type"),
+            reader.GetString("cardholder_name"),
+            reader.GetString("user_id")
+          ));
+        }
       }
-
-      return result;
     }
+    return result;
   }
+
+
 }
+
