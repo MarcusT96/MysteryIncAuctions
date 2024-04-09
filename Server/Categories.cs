cbaa9bdc0cccc21f66fef6b9b3bd49b049cba4f0
TommyJX <ttommywang@gmail.com>
2024-04-09 16:04:52 +0200

    implemented post and get methods

--- Server/Categories.cs
+++ Server/Categories.cs
@@ -3,14 +3,11 @@ using MySql.Data.MySqlClient;
 
 public class CategoryOptions
 {
-  public record Category(
-    int Id,
-    string CategoryName);
+  public record Category(int Id, string CategoryName);
 
   public static List<Category> GetCategories()
   {
-    List<Category> result = new();
-
+    List<Category> result = new List<Category>();
     string connectionString = "server=localhost;port=3306;database=mystery_practice;user=root;password=mypassword";
 
     using (var connection = new MySqlConnection(connectionString))
@@ -27,7 +24,38 @@ public class CategoryOptions
         }
       }
     }
-
     return result;
+  }
+  public static async Task<bool> CreateCategory(Category category)
+  {
+    string connectionString = "server=localhost;port=3306;database=mystery_practice;user=root;password=mypassword";
+    await using (var connection = new MySqlConnection(connectionString))
+    {
+      var query = "INSERT INTO Categories (CategoryName) VALUES (@CategoryName);";
+      await using (var command = new MySqlCommand(query, connection))
+      {
+        command.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+        await connection.OpenAsync();
+        var result = await command.ExecuteNonQueryAsync();
+        return result > 0;
+      }
+    }
+  }
+
+  public static async Task<bool> UpdateCategory(int id, Category category)
+  {
+    string connectionString = "server=localhost;port=3306;database=mystery_practice;user=root;password=mypassword";
+    await using (var connection = new MySqlConnection(connectionString))
+    {
+      var query = "UPDATE Categories SET CategoryName = @CategoryName WHERE Id = @Id;";
+      await using (var command = new MySqlCommand(query, connection))
+      {
+        command.Parameters.AddWithValue("@Id", id);
+        command.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+        await connection.OpenAsync();
+        var result = await command.ExecuteNonQueryAsync();
+        return result > 0;
+      }
+    }
   }
 }
