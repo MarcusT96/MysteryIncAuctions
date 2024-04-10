4317aad7947e5c895b3058a760b6d03df23ec959
loken156 <jonathan.karlsson@mail.com>
2024-04-10 12:33:21 +0200

    fixat auctionlists värden och lagt till i lokal bas

--- Server/Program.cs
+++ Server/Program.cs
@@ -25,7 +25,8 @@ app.MapGet("/categories", CategoryOptions.GetCategories);
 app.MapPost("/bids", async (HttpContext context) => await Bid.AddBid(context));
 app.MapGet("/payment_options", PaymentOptions.PaymentOpts);
 app.MapGet("/users/{id:int}", async (int id) => await User.GetUserById(id));
-app.MapGet("/boxes", async () => await Boxes.All());app.MapGet("/users", User.GetUsers);
+app.MapGet("/boxes", async () => await Boxes.All());
+app.MapGet("/users", User.GetUsers);
 app.MapPost("/users", async (User.UserRecord newUser) => await User.CreateUser(newUser));
 app.MapPut("/users/{id:int}", async (int id, User.UserRecord updatedUser) => await User.UpdateUser(id, updatedUser));
 
