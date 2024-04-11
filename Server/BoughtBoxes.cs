eb0663f5fcb1319a8bb8f2b9718234f1a691d51b
TommyJX <ttommywang@gmail.com>
2024-04-11 11:40:45 +0200

    Implemented PUT method

--- Server/BoughtBoxes.cs
+++ Server/BoughtBoxes.cs
@@ -66,4 +66,29 @@ public class BoughtBoxesOptions
     }
   }
 
+  public static async Task<bool> UpdateBoughtBox(int id, BoughtBox box)
+  {
+    string connectionString = "server=localhost;port=3306;database=mystery_practice;user=root;password=mypassword";
+    await using (var connection = new MySqlConnection(connectionString))
+    {
+      var query = "UPDATE BoughtBoxes SET Name = @Name, Weight = @Weight, Price = @Price, Time = @Time, Description = @Description, Image = @Image, buyer_id = @buyer_id, Paid = @Paid, Delivered = @Delivered WHERE Id = @Id;";
+      await using (var command = new MySqlCommand(query, connection))
+      {
+        command.Parameters.AddWithValue("@Id", id);
+        command.Parameters.AddWithValue("@Name", box.Name);
+        command.Parameters.AddWithValue("@Weight", box.Weight);
+        command.Parameters.AddWithValue("@Price", box.Price);
+        command.Parameters.AddWithValue("@Time", box.Time);
+        command.Parameters.AddWithValue("@Description", box.Description);
+        command.Parameters.AddWithValue("@Image", box.Image);
+        command.Parameters.AddWithValue("@buyer_id", box.BuyerId);
+        command.Parameters.AddWithValue("@Paid", box.Paid);
+        command.Parameters.AddWithValue("@Delivered", box.Delivered);
+
+        await connection.OpenAsync();
+        var result = await command.ExecuteNonQueryAsync();
+        return result > 0;
+      }
+    }
+  }
 }
