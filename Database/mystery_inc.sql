448e6bfcc9dc8a00c4f5cabfd91def61b322378c
HellgrenR <rallenolltva@gmail.com>
2024-05-10 16:12:23 +0200

    Changed create statement

--- Database/mystery_inc.sql
+++ Database/mystery_inc.sql
@@ -15,7 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ 'mystery_inc' /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+CREATE DATABASE IF NOT EXISTS mystery_inc;
+
+USE mystery_inc;
 
 --
 -- Table structure for table `bids`
