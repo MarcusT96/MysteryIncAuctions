b23989bc46e5255f554bb13039bbe594714fd5ca
Tommy Wang <ttommywang@gmail.com>
2024-05-15 15:26:13 +0200

    Changed name of feature file
    In order to have a better overview of the changes, I renamed the feature file.

--- Cypress/specs/step_definitions/checkAdminPanelMain.js
+++ Cypress/specs/step_definitions/checkAdminPanelMain.js
@@ -1,21 +1,5 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-When('I click the {string} in the navbar', (a) => {
-  cy.get('div.app__navbar-login > .app__navbar-login').should('be.visible').click()
-});
-
-Then('I enter my admin {string}', (a) => {
-  cy.get('[type="email"]').click().type('tommy@example.com')
-});
-
-Then('I also enter my {string}', (a) => {
-  cy.get('[type="password"]').click().type('hej123')
-});
-
-Then('I click the {string} button', (a) => {
-  cy.get('[type="submit"]').click()
-});
-
 Then('then I click the {string} button', (a) => {
   cy.get('.p__opensans-profil').click()
   cy.url().should('include', '/profile');
