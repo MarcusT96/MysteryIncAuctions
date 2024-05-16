39669c28bfe80748e5f9d8eb1d28005d283b1351
HellgrenR <rallenolltva@gmail.com>
2024-05-16 12:54:37 +0200

    Fixing my tests

--- Cypress/specs/paymentopts.feature
+++ Cypress/specs/paymentopts.feature
@@ -1,15 +1,7 @@
 Feature: Payment options
 
-  Scenario: Login with valid credentials
-    Given I am on the homepage
-    When I click on the "Login" button in the navigation bar
-    And I fill in my email and password with valid credentials
-    And I click the submit button
-    Then I should be back on the homepage
-
   Scenario: Add payment option
-    Given that I am on the homepage
-    And I am logged in
+    Given I am logged in
     When I click on min profile
     And I go to betalningsalternativ
     And I click on lägg till betalningsalternativ
--- Cypress/specs/step_definitions/paymentopts.js
+++ Cypress/specs/step_definitions/paymentopts.js
@@ -1,41 +1,22 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-/* No duplicate steps, this one already in login.js
-Given('I am on the homepage', () => {});*/
-
-/* No duplicate steps, this one already in login.js
-When('I click on the {string} button in the navigation bar', (a) => {});*/
-
-/* No duplicate steps, this one already in login.js
-When('I fill in my email and password with valid credentials', () => {});*/
-
-/* No duplicate steps, this one already in login.js
-When('I click the submit button', () => {});*/
-
-/* No duplicate steps, this one already in login.js
-Then('I should be back on the homepage', () => {});*/
-
-Given('that I am on the homepage', () => {
-  // TODO: implement step
-});
-
 /* No duplicate steps, this one already in bid.js
 Given('I am logged in', () => {});*/
 
 When('I click on min profile', () => {
-  // TODO: implement step
+  cy.get('.p__opensans-profil').click()
 });
 
 When('I go to betalningsalternativ', () => {
-  // TODO: implement step
+  cy.get('.sidebar > :nth-child(2)').click()
 });
 
 When('I click on lägg till betalningsalternativ', () => {
-  // TODO: implement step
+  cy.get('.paymentopt-addpay').click()
 });
 
 When('I select the type', () => {
-  // TODO: implement step
+  cy.get('#type').click()
 });
 
 When('I add my information', () => {
