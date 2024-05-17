5d32566140146054d9eaf2a039e26fb7a310476e
MarcusT96 <marcus.thure@hotmail.com>
2024-05-17 10:06:43 +0200

    för  fan

--- Client/src/pages/MyHomePage.jsx
+++ Client/src/pages/MyHomePage.jsx
@@ -9,7 +9,7 @@ function MyHomePage() {
 
       <div className="homepage-desc-and-revs">
         <div className="homepage-description">
-          <h2>Mystery Inc.</h2>
+          <h2>Mystery Inc fö fan</h2>
           <p>MysteryInc är inte bara skapare av spännande mysterielådor utan också mysteriska vidunder! Dom fungerar också som en spännande auktionssida. Här kan du delta i budgivningar för olika mystery boxes med olika teman. Varje box är noggrant kuraterad och kan innehålla allt från senaste teknikprylar till exklusiva handplockade upplevelser. Genom att delta i budgivningen får du chansen att vinna en unik skatt full av överraskningar, och det är ett sätt för äventyrsentusiaster att uppleva det oväntade direkt från MysteryInc till din dörr.</p>
         </div>
         <HomePageReviews/>
